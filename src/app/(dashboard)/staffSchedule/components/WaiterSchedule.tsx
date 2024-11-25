ba7baeb3d37615cd8228e0f86554affdc88aa44a
Thang <thang27302@gmail.com>
2024-11-25 08:47:16 +0700

    remove disable add

--- src/app/(dashboard)/staffSchedule/components/WaiterSchedule.tsx
+++ src/app/(dashboard)/staffSchedule/components/WaiterSchedule.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
@@ -157,7 +158,7 @@ function WaiterSchedule() {
                           Nhân viên: {shiftCount?.employeeCount}
                         </div>
                         <div className="flex flex-col space-y-2">
-                          {!isEditableDate ? (
+                          {/* {!isEditableDate ? (
                             <Button
                               variant="outline"
                               size="sm"
@@ -186,7 +187,25 @@ function WaiterSchedule() {
                               <PlusIcon className="h-4 w-4 mr-2" />
                               Thêm
                             </Button>
-                          )}
+                          )} */}
+                          <Button
+                            variant="outline"
+                            size="sm"
+                            className="bg-white"
+                            onClick={() => {
+                              setScheduleInfo({
+                                date: format(day, "EEEE,dd/MM/yyyy", {
+                                  locale: vi,
+                                }),
+                                shiftTime: `${shift.startTime} - ${shift.endTime}`,
+                                shiftId: shift.id,
+                              });
+                              setOpenAdd(true);
+                            }}
+                          >
+                            <PlusIcon className="h-4 w-4 mr-2" />
+                            Thêm
+                          </Button>
 
                           <Button
                             variant="outline"
